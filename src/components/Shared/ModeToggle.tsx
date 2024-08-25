49b2498936fec80c28e0e0c5a59672e46e557dde
noyon-podder <noyon.podder7@gmail.com>
2024-08-25 09:50:24 +0600

    toggle button customize

--- src/components/Shared/ModeToggle.tsx
+++ src/components/Shared/ModeToggle.tsx
@@ -1,19 +1,22 @@
-import { Moon, Sun } from "lucide-react";
+// import { Moon, Sun } from "lucide-react";
+import Sun from "/sun.png";
+import Moon from "/moon.png";
 
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+// import { Button } from "@/components/ui/button";
+// import {
+//   DropdownMenu,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+//   DropdownMenuTrigger,
+// } from "@/components/ui/dropdown-menu";
 import { useTheme } from "@/context/ThemeProvider";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
 
   return (
-    <DropdownMenu>
+    <>
+      {/* <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
@@ -32,6 +35,29 @@ export function ModeToggle() {
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
-    </DropdownMenu>
+    </DropdownMenu> */}
+
+      <div className="flex items-center justify-between w-[60px] h-[25px] border border-color-darkBaseColor rounded-[20px] p-1 bg-color-darkBaseColor dark:bg-white dark:border-white  cursor-pointer relative ">
+        <img
+          src={Moon}
+          alt=""
+          width={20}
+          height={20}
+          onClick={() => setTheme("dark")}
+        />
+        <div
+          className={`w-5 h-5 bg-white dark:bg-color-darkBaseColor  rounded-full absolute  transition-transform duration-300 ease-out ${
+            theme === "dark" ? "left-1" : "right-1"
+          }`}
+        ></div>
+        <img
+          src={Sun}
+          alt=""
+          width={20}
+          height={20}
+          onClick={() => setTheme("light")}
+        />
+      </div>
+    </>
   );
 }
--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from "./ModeToggle";
 
 const Navbar = () => {
   return (
-    <div className=" dark:border-b dark:bg-transparent py-5 bg-[#f2f2f2]">
+    <div className=" dark:border-b dark:bg-transparent py-5 bg-color-baseColor">
       <div className="container flex items-center justify-between">
         <h2 className="text-2xl dark:text-white text-red-700">Logo</h2>
         <ModeToggle />
